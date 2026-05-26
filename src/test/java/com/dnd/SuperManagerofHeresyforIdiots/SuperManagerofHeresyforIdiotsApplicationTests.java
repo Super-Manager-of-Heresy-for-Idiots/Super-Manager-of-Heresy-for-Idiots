8c1560698408922a784fb444c7500e8871feac92
cryingloli <clashofsparta.cof@gmail.com>
2026-05-26 15:30:34 +0300

    SWAG??

---
+++ src/test/java/com/dnd/SuperManagerofHeresyforIdiots/SuperManagerofHeresyforIdiotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.SuperManagerofHeresyforIdiots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperManagerofHeresyforIdiotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
